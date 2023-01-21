45506df2d4846ec0214b8cf33d2ba07f3f163bdb
attilamedina <66525714+attilamedina@users.noreply.github.com>
2023-01-21 07:57:31 +0100

    Update index.php

--- Application/index.php
+++ Application/index.php
@@ -31,7 +31,7 @@
                 <div class="success-text">Success!</div>
                 <div class="description line-1">
 				<?php
-				echo "Attila Medina pipeline test excercise with orchestrator. STAGING";
+				echo "Attila Medina pipeline test excercise with orchestrator. STAGING FIRST!";
 				?>
 				</div>
                 <div class="description line-2">
