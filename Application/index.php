a633ce458e9b4af39705d7ce8b5cd06e318664db
attilamedina <66525714+attilamedina@users.noreply.github.com>
2023-01-22 08:07:15 +0100

    Update index.php

--- Application/index.php
+++ Application/index.php
@@ -31,7 +31,7 @@
                 <div class="success-text">Welcome!</div>
                 <div class="description line-1">
 				<?php
-				echo "Attila Medina pipeline test assesment. Please validate the staging slot to deploy to production.";
+				echo "Attila Medina pipeline test assesment. Please validate the staging env to deploy to production.";
 				?>
 				</div>
                 <div class="description line-2">
