8ba9e3ac808b6f9bda2697c71ccb89ae19719409
attilamedina <66525714+attilamedina@users.noreply.github.com>
2023-01-22 06:22:43 +0100

    Update index.php

--- Application/index.php
+++ Application/index.php
@@ -37,7 +37,7 @@
                 <div class="description line-2">
 				<?php
 				$appType = "PHP";
-				echo "This $appType app is up and running on Azure.";
+				echo "This $appType app is up and running on Azure .";
 				?>
 				</div>
                 <div class="next-steps-container">
